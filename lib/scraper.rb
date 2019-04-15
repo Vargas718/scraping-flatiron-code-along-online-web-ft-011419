de2ce791eb5a34084890baf4d6354eefb983f550
Rafael Vargas <vargas718@gmail.com>
2019-04-15 16:30:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require "pry"
 
 require_relative './course.rb'
 
@@ -14,9 +15,18 @@ class Scraper
         puts "  Description: #{course.description}"
       end
     end
-  end
+   end  
+   
+    def get_page
+     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    # binding.pry
+    end  
+    def get_courses 
+      doc.css(".post")
+    end 
   
 end
 
 
 
+Scraper.new.get_page
